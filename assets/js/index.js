f1eeee753bb2cf643bbb3a29f0fbfed797a2a620
Ighor Alves <ighoralc@gmail.com>
2022-07-10 03:03:58 -0300

    inicio

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+const btnMobile = document.getElementById('Btn-mobile'); 
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('Nav');
+  nav.classList.toggle('active'); 
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
