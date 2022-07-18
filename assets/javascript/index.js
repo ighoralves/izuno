b61264f039cb9b10c05e4aab84e278780256e8e0
Luan Ramos <luan@apiki.com>
2022-07-18 16:46:41 -0300

    novos arquivos

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,25 +1,2 @@
-const btnMobile = document.getElementById('Btn-mobile'); 
-
-function toggleMenu(event) {
-  if (event.type === 'touchstart') event.preventDefault();
-  const nav = document.getElementById('Nav');
-  nav.classList.toggle('active'); 
-}
-
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
-
-import Swiper from 'swiper';
-  
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 'auto',
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-  },
-});
-
-export {swiper}; 
+import './menu';
+import './swiper';
